10d9db80f86b547166fef1113aae1ac871686343
deveploper99 <rtxdeveploper99@gmail.com>
2026-02-25 15:26:30 +0600

    practis quals and hashcode

---
+++ src/main/java/org/rtxrassel/custom/Person.java
@@ -0,0 +1,26 @@
+package org.rtxrassel.custom;
+
+import java.util.Objects;
+
+public class Person {
+    int id;
+    String name;
+
+     public Person(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this ==o) return true;
+        if (o==null || getClass()!=o.getClass()) return false;
+        Person person =(Person) o;
+        return id ==person.id;
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(id);
+    }
+}
