eb435a42065be056a55fd8656afcffa1d39d3a08
deveploper99 <rtxdeveploper99@gmail.com>
2026-02-28 12:21:02 +0600

    arraylist max and min number find

---
+++ src/main/java/org/rtxrassel/arrayListPractis/biggerstNumber.java
@@ -0,0 +1,36 @@
+package org.rtxrassel.arrayListPractis;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class biggerstNumber {
+  public   static void main(String[] args) {
+
+      List<Integer> number = new ArrayList<>();
+
+      for (int i=1;i<=10;i++){
+          number.add(i);
+      }
+//      int max = number.get(0);
+//      int min = number.get(0);
+
+//      if (!number.isEmpty()){
+//          for (Integer s : number){
+//              if (s>=max){
+//                  max = s;
+//              }
+//
+//              if (s<=min){
+//                  min = s;
+//              }
+//          }
+//      }
+
+      int max = Collections.max(number);
+      int min = Collections.min(number);
+
+      System.out.println("Max = " + max);
+      System.out.println("Min = " + min);
+    }
+}
