b7ac6e789a02f60c8e9754bee1b5136255d4410f
deveploper99 <rtxdeveploper99@gmail.com>
2026-02-28 12:40:50 +0600

    arraylist max and min number find and second largest number find

--- src/main/java/org/rtxrassel/arrayListPractis/biggerstNumber.java
+++ src/main/java/org/rtxrassel/arrayListPractis/biggerstNumber.java
@@ -5,32 +5,51 @@ import java.util.Collections;
 import java.util.List;
 
 public class biggerstNumber {
-  public   static void main(String[] args) {
+    public static void main(String[] args) {
 
-      List<Integer> number = new ArrayList<>();
+        List<Integer> number = new ArrayList<>();
 
-      for (int i=1;i<=10;i++){
-          number.add(i);
-      }
-//      int max = number.get(0);
-//      int min = number.get(0);
+        for (int i = 1; i <= 10; i++) {
+            number.add(i);
+        }
+        int max = number.get(0);
+        int min = number.get(0);
 
-//      if (!number.isEmpty()){
-//          for (Integer s : number){
-//              if (s>=max){
-//                  max = s;
-//              }
-//
-//              if (s<=min){
-//                  min = s;
-//              }
-//          }
-//      }
+        if (!number.isEmpty()) {
+            for (Integer s : number) {
+                if (s >= max) {
+                    max = s;
+                }
 
-      int max = Collections.max(number);
-      int min = Collections.min(number);
+                if (s <= min) {
+                    min = s;
+                }
+            }
+        }
 
-      System.out.println("Max = " + max);
-      System.out.println("Min = " + min);
+
+//      int max = Collections.max(number);
+//      int min = Collections.min(number);
+
+        System.out.println("Max = " + max);
+        System.out.println("Min = " + min);
+
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (Integer m : number) {
+            if (m > largest) {
+                secondLargest = largest;
+                largest = m;
+            } else if (m>secondLargest && m !=largest) {
+                secondLargest = m;
+                
+            }
+        }
+
+
+        System.out.println("Largest = " + largest);
+        System.out.println("Second Largest = " + secondLargest);
     }
 }
